616dc6a6bbe35b9efb7aa8ff8bf8b560945cd950
Tim Speckhals <timmy14085@gmail.com>
2017-11-17 12:29:48 +0000

    Create Example.php

---
+++ Example.php
@@ -0,0 +1,33 @@
+<?php
+
+// Downloading The Feed
+$ch = curl_init();
+curl_setopt_array($ch, [
+	CURLOPT_URL => "FEED URL HERE",
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true
+]);
+$rawfeed = curl_exec($ch);
+curl_close($ch);
+$rawfeed = str_replace(" & ", " &amp; ", $rawfeed); // Some feeds may have unencoded amps, which can result in errors.
+
+// Parsing the Feed
+$feed = Feed::fromString($rawfeed);
+unset($rawfeed);
+if(!$feed->isTypeSupported())
+{
+	die($feed->type." is not supported!\n");
+}
+
+// Listing the Feed's Articles
+echo "The 10 most recent articles of ".$feed->name.":\n";
+$i = 0;
+foreach($feed->getArticles() as $article)
+{
+	echo "- ".$article->getName();
+	if($i++ == 10)
+	{
+		break;
+	}
+}
